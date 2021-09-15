930548756bfcf7e0f59f5edabfe2a1904682668a
ishranm9@gmail.com <ishranm9@gmail.com>
2021-09-15 16:32:49 +0200

    bug correction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Home.jsx";
+import hotelcity from "./components/Hotels.jsx";
+import hotel from "./components/HotelPage.jsx";
+import Navbar from "./core/Navbar.jsx";
+import Error404 from "./components/Error404";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
-      <h>hello</h>
+       <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/hotels/:city" component={hotelcity} />
+            <Route path="/hotel/:id" component={hotel} />
+            <Route path="*" component={Error404} />
+          </Switch>
+        </BrowserRouter>
     </div>
   )
 }
